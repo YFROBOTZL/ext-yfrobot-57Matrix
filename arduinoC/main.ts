db7716c42948e05c9fd288de415858874d954166
YFROBOTZL <yfrobot-zl@foxmail.com>
2022-03-04 16:08:48 +0800

    init

--- arduinoC/main.ts
+++ arduinoC/main.ts
@@ -121,26 +121,77 @@ namespace dotmatrix57 {
         Generator.addCode(`matrix57.set(${bright});`);
     }
 
+    
     //% extenralFunc
     export function getBuiltinFunc_() {
         return [
-            {matrix: "{B00000000,B00001000,B00001110,B00001010,B00001000,B00111000,B00111000,B00000000}"},
-            {matrix: "01010110101011010101101010110101011"},
-            {matrix: "01010110101011010101101010110101011"},
-            {matrix: "01010110101011010101101010110101011"},
-            {matrix: "01010110101011010101101010110101011"}
+            {"matrix":"0000000000000111011100010001000101000000000110000000001100000000010100000001000100000100000100010000000101000000000100000"},
+            {"matrix":"0000000000000111011100011111111101111111111111111111111111111111110111111111000111111100000111110000000111000000000100000"},
+            {"matrix":"0000000000000000000000011000001001111000101011110010001011000000000000000000000000000000000111110000000000000000000000000"},
+            {"matrix":"0000000000000000000000010100010100010000010000000000000000000000000010000010000010001000000011100000000000000000000000000"},
+            {"matrix":"0000000000000000000000000000000000110000011001100000110000000000000001111100000100000100010000000100000000000000000000000"},
+            {"matrix":"0000000000000000000000010000000100010000010000010001000001000001000100000001000000000000000011100000001000100000000000000"},
+            {"matrix":"0100000010001110000111110000011001100000110001100000110010100001010101000010111100001110010000001000000000000000001110000"},
+            {"matrix":"0000000000000000000000110110110111111101111101110001110001000001000000000000000000000000000100010000000111000000000000000"},
+            {"matrix":"0000000000000000000000011110111100011000110000110001100001100011000010000010000000000000000011100000001000100000000000000"}
+        ]
+    }
+    //% block="BadgeDisplay Matrix [MT] At X[X]" blockType="command"
+    //% MT.shadow="matrix" MT.params.row=11 MT.params.column=11 MT.defl="0000000000000111011100010001000101000000000110000000001100000000010100000001000100000100000100010000000101000000000100000"
+    //% X.shadow="range" X.params.min="1" X.params.max="44" X.defl="1"
+    //% MT.params.builtinFunc="getBuiltinFunc_"
+    export function setMatrix(parameter: any, block: any) {
+        let matrix = parameter.MT.code;
+        Generator.addInclude("addIncludeHuizhang","#include <Huizhang.h>");
+        Generator.addObject("addhz_matrix","uint16_t",`hz_matrix[11];`);
+        let hz_matrix:any[] = [0,0,0,0,0,0,0,0,0,0,0];
+        for (let i = 0; i < 11; i++) {
+            for (let j = 0; j < 11; j++) {
+                hz_matrix[i] += matrix.charAt(i + 11 * j) << (10 - j);
+            }
+            if (hz_matrix[i] < 0x10) {
+                hz_matrix[i] = "0x00" + hz_matrix[i].toString(16);
+            } else if (hz_matrix[i] < 0x100) {
+                hz_matrix[i] = "0x0" + hz_matrix[i].toString(16);
+            } else {
+                hz_matrix[i] = "0x" + hz_matrix[i].toString(16);
+            }
+            if (i % 2) Generator.addCode(`hz_matrix[${i-1}] = ${hz_matrix[i-1]};hz_matrix[${i}] = ${hz_matrix[i]};`);
+            else if (i == 10) Generator.addCode(`hz_matrix[${i}] = ${hz_matrix[i]};`);
+        }
+        let x = parameter.X.code;
+        Generator.addCode(`hz.SendMatrix(hz_matrix, ${x});`);
+    }
+
+    //% extenralFunc
+    export function getBuiltinFuncx_() {
+        return [
+            {matrix: "00111000111110011111000111000001000"},
+            {matrix: "00000000001000001110000010000000000"},
+            {matrix: "00010000011100011111000111000001000"},
+            {matrix: "01101100110110000000001000100011100"}
         ]
     }
 
     //% block="57 dot matrix display [DMARRAY]" blockType="command"
-    //% DMARRAY.shadow="matrix" DMARRAY.params.row=5 DMARRAY.params.column=7 DMARRAY.split="B" DMARRAY.defl="01010110101011010101101010110101011"
-    //% DMARRAY.params.builtinFunc="getBuiltinFunc_"
+    //% DMARRAY.shadow="matrix" DMARRAY.params.row=5 DMARRAY.params.column=7 DMARRAY.split=true DMARRAY.defl="01101100110110000000001000100011100"
+    //% DMARRAY.params.builtinFunc="getBuiltinFuncx_"
     // http://download.dfrobot.top/extList.json
     export function displayArray(parameter: any, block: any) {
         let dmarray = parameter.DMARRAY.code;
-        let dma = dmarray;
-        Generator.addObject("matrix57array", `const uint8_t`, `matrix57(${clk},${dio});`);
-        Generator.addCode(`matrix57.display(${dmarray}[0]);`);
+        let dm_matrix:any[] = [];
+        let number_matrix = 0;
+        // let xx = dmarray.slice(0, 4);
+        for (let i = 0; i < 5; i++) {
+            dm_matrix[i] = dmarray.slice(0+i*7, 7+i*7);
+        }
+        // Generator.addObject("matrix57array", `const uint8_t`, `matrix57[][7] = {\n  {B${xx}}\n};`);
+        
+        // Generator.addInclude("includeMatrix57", `#include \"Matrix57.h\"`);
+        Generator.addInclude(`matrix57array${dmarray}`, `const uint8_t matrix57_${dmarray}[][5] = {\n  {B${dm_matrix[0]},B${dm_matrix[1]},B${dm_matrix[2]},B${dm_matrix[3]},B${dm_matrix[4]}}\n};`);
+        // Generator.addObject(`matrix57array${dmarray}`, `const uint8_t`, `matrix57_${dmarray}[][7] = {\n  {B${dm_matrix[0]},B${dm_matrix[1]},B${dm_matrix[2]},B${dm_matrix[3]},B${dm_matrix[4]}}\n};`);
+        Generator.addCode(`matrix57.display(matrix57_${dmarray}[${number_matrix}]);`);
+        number_matrix++;
     }
 
     //% extenralFunc2
